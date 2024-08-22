0fa6dfbce489fbe13e68f4274ddf04142904208e
q4artz <116222352+q4artz@users.noreply.github.com>
2024-08-21 20:48:15 -0700

    Add files via upload

---
+++ Maragi-Framework/Services/IAgentService.cs
@@ -0,0 +1,39 @@
+﻿using Maragi_Framework.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Maragi_Framework.Services
+{
+    // resembles IListenerService closely
+    public interface IAgentService
+    {
+        void AddAgent(Agent agent);
+        // can return a list / array / any type
+        IEnumerable<Agent> GetAgents();
+        Agent GetAgent(string id);
+        void RemoveAgent(Agent agent);
+    }
+    public class AgentService : IAgentService
+    {
+        private readonly List<Agent> _agents = new();
+        public void AddAgent(Agent agent)
+        {
+            _agents.Add(agent);
+        }
+
+        public IEnumerable<Agent> GetAgents()
+        {
+            return _agents;
+        }
+
+        public Agent GetAgent(string id)
+        {
+            return GetAgents().FirstOrDefault(a => a.Metadata.Id.Equals(id));
+        }
+
+        public void RemoveAgent(Agent agent)
+        {
+           _agents.Remove(agent);
+        }
+    }
+}
