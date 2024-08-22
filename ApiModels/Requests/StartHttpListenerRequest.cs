712b748dd84023fbfc5e37d0e3ae0834e9a8c9fd
q4artz <116222352+q4artz@users.noreply.github.com>
2024-08-21 17:38:51 -0700

    Add files via upload

--- ApiModels/Requests/StartHttpListenerRequest.cs
+++ ApiModels/Requests/StartHttpListenerRequest.cs
@@ -2,6 +2,7 @@
 {
     public class StartHttpListenerRequest
     {
+        // creating seperate project so other libraries can reference it -- client can reference this ApiModel
         public string Name { get; set; }
         public int BindPort { get; set; }
     }
