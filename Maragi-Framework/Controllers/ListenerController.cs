f98e6bb44f2593107d04ff8a6f3181e54b600335
q4artz <116222352+q4artz@users.noreply.github.com>
2024-08-21 17:39:12 -0700

    Add files via upload

--- Maragi-Framework/Controllers/ListenerController.cs
+++ Maragi-Framework/Controllers/ListenerController.cs
@@ -9,6 +9,7 @@ namespace Maragi_Framework.Controllers
     [Route("[controller]")]
     public class ListenersController : ControllerBase
     {
+        // write API to interact with Listener Service
         private readonly IListenerService _listeners;
 
         public ListenersController(IListenerService listeners)
@@ -23,9 +24,12 @@ namespace Maragi_Framework.Controllers
             return Ok(listeners);
         }
 
+        // will be populated with swagger -- swagger will pick this up
+        // if name match it will know GetListener's string "name" is HttpGet "name"
         [HttpGet("{name}")]
         public IActionResult GetListener(string name)
         {
+            // in IListenerService we set FirstOrDefault == if listener don't exist it will return null
             var listener = _listeners.GetListener(name);
             if (listener is null) return NotFound();
 
@@ -35,6 +39,7 @@ namespace Maragi_Framework.Controllers
         [HttpPost]
         public IActionResult StartListener([FromBody] StartHttpListenerRequest request)
         {
+            // Remove Using System.Net; Add using TeamServer.Modules;
             var listener = new HttpListener(request.Name, request.BindPort);
             listener.Start();
 
