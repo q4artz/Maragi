aaa58de692608cddf1088382c80c9dedfbb0895b
q4artz <116222352+q4artz@users.noreply.github.com>
2024-08-29 01:33:43 -0700

    Add files via upload

---
+++ ApiModels/Requests/TaskAgentRequest.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ApiModels.Requests
+{
+    public class TaskAgentRequest
+    {
+        public string Command { get; set; }
+        public string[] Arguements { get; set; }
+
+        // for execute-assembly
+        public byte[] File { get; set; }
+    }
+}
